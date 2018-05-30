612ccad5d4244373477d08fb0d8f31a1d0cea4f9
Minh Hieu Dang Vu <hieudvm@gmail.com>
2018-05-30 20:49:51 +0700

    GUI Ver 0.9

--- gui.py
+++ gui.py
@@ -1,15 +1,19 @@
 from Tkinter import *
+from PIL import ImageTk, Image
 import tkFileDialog as filedialog
 import ttk
 from utility import loadPickle
+import cv2
 
 
 class GUI(object):
 
-    def __init__(self):
+    def __init__(self, models, class_labels):
         self.root = Tk()
         self.setup_gui()
-        self.model = None
+        self.models = models
+        self.selected_model = models[0]
+        self.classifier_labels = class_labels
 
     def setup_gui(self):
 
@@ -23,14 +27,24 @@ class GUI(object):
         self.train_file.grid(row=0, column=1, padx=2, pady=4)
         self.train_file.bind("<Button-1>", self.choose_directory)
 
-        self.classifier = ttk.Combobox(self.root)
-        #nb = loadPickle('./model/naive_bayes/multinomial_bayes_bow_500')                
+        self.classifier = ttk.Combobox(self.root)                       
         self.classifier['value'] = ("Naives Bayes","SVM")
+        self.classifier.bind("<<ComboboxSelected>>", self.selected_model())
         self.classifier.grid(row=1, column=1, padx=2, pady=4)
+        self.classifier.current(0)
 
         self.button = Button(self.root, text="Image input", command=self.choose_image)
-        self.button.grid(row=2, padx=2, pady=4)    
+        self.button.grid(row=2, padx=2, pady=4)
 
+        self.img = ImageTk.PhotoImage("RGB", (300,300))
+        self.panel = Label(self.root, image=self.img)
+        self.panel.grid(row=2, column=1)    
+        
+        self.class_result = Label(self.root, text="Class : ")
+        self.class_result.grid(row=3)
+
+    def set_select_model(self, event):
+        self.selected_model = self.models[self.classifier.get()]
 
     def choose_directory(self, event):
         folder = filedialog.askdirectory()
@@ -38,12 +52,26 @@ class GUI(object):
         self.train_file.insert("1.0", folder.split("/")[-1])
 
     def choose_image(self):
-        folder = filedialog.askopenfilename()
+        file = filedialog.askopenfilename()
+        img = ImageTk.PhotoImage(Image.open(file))
+        self.panel.configure(image=img)
+        panel.image = img
+        image = cv2.imread(file)
+        result = model.classify(image)
+        class_label = self.classifier_labels[result]
+        self.class_result.configure(text="Class : " + class_label)
+
+
 
     def run(self):
         self.root.mainloop()
 
 
 if __name__ == '__main__':
-    gui = GUI()
+    bow = loadPickle('./bag-of-words/bow_500').cluster_centers_
+    class_labels = loadPickle('./cifar-10-batches-py/batches.meta')
+    print class_labels
+    nb = loadPickle('./models/naive_bayes/multinomial_bayes_bow_500')
+    models = [nb]
+    gui = GUI(models, class_labels)
     gui.run()
