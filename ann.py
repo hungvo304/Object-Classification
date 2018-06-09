455a78e96afe22249ef39ddcb059ffd825cc14de
dantevergil304 <voquochung304@gmail.com>
2018-06-09 12:39:50 +0700

    Rewrite ann.py to class type

--- ann.py
+++ ann.py
@@ -1,9 +1,6 @@
-from utility import loadPickle, writePickle
+from utility import loadPickle, writePickle, stratifiedSplitTrainSet
 from svm import getTrainDataAndLabel
-from bow import encodeBatch
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import StratifiedShuffleSplit
-import numpy as np
 import torch
 import copy
 
@@ -20,6 +17,63 @@ class ANN(torch.nn.Module):
         return y_pred
 
 
+class NeuralNet():
+    def __init__(self, N, D_in, H, D_out):
+        self.N = N
+        self.D_in = D_in
+        self.H = H
+        self.D_out = D_out
+        self.model = ANN(D_in, H, D_out)
+
+    def fit(self, trn_encoded, labels):
+        # Split data to train set and dev set
+        trn_data, trn_labels, dev_data, dev_labels = stratifiedSplitTrainSet(
+            trn_encoded, labels, num_splits=1)
+
+        # Convert numpy array to torch tensor
+        X = torch.from_numpy(trn_data).float()
+        Y = torch.from_numpy(trn_labels).long()
+        X_dev = torch.from_numpy(dev_data).float()
+        Y_dev = torch.from_numpy(dev_labels).long()
+
+        # Train model
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(
+            self.model.parameters(), lr=1e-4, momentum=0.9)
+        acc_go_down = 0
+        max_dev_acc = float('-inf')
+        bestModel = None
+        for t in range(500):
+            for iteration in range(X.shape[0] // self.N):
+                start = iteration * self.N
+                end = start + self.N
+                x = X[start:end, :]
+                y_pred = self.model(x)
+
+                y = Y[start:end]
+                loss = criterion(y_pred, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            Y_dev_pred = self.model(X_dev)
+            dev_acc = 100 * \
+                accuracy_score(Y_dev_pred.data.numpy().argmax(
+                    axis=1), Y_dev.numpy())
+            print t, 'dev accuracy', dev_acc
+
+            if dev_acc > max_dev_acc:
+                acc_go_down = 0
+                bestModel = copy.deepcopy(self.model)
+            else:
+                acc_go_down += 1
+                if acc_go_down == 20:
+                    break
+
+        self.model = copy.deepcopy(bestModel)
+
+
 if __name__ == '__main__':
     # get bag of words
     bow = loadPickle('./bag-of-words/bow_500').cluster_centers_
@@ -27,67 +81,11 @@ if __name__ == '__main__':
     # define #-dimensions
     N, D_in, H, D_out = 50, 500, 1000, 10
 
+    # create neural network
+    ann = NeuralNet(N, D_in, H, D_out)
+
+    # get train data
     trn_encoded, labels = getTrainDataAndLabel(bow)
 
-    # Split data to train set and dev set
-    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-    for train_index, test_index in split.split(trn_encoded, labels):
-        trn_data = trn_encoded[train_index]
-        trn_labels = labels[train_index]
-
-        dev_data = trn_encoded[test_index]
-        dev_labels = labels[test_index]
-
-    # print trn_data.shape, dev_data.shape
-
-    tst = loadPickle('./cifar-10-batches-py/test_batch')
-    tst_encoded = encodeBatch(tst, bow)
-
-    X = torch.from_numpy(trn_data).float()
-    Y = torch.from_numpy(trn_labels).long()
-    X_dev = torch.from_numpy(dev_data).float()
-    Y_dev = torch.from_numpy(dev_labels).long()
-    X_test = torch.from_numpy(np.array(tst_encoded)).float()
-    Y_test = torch.from_numpy(np.array(tst['labels'])).long()
-
-    model = ANN(D_in, H, D_out)
-
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-    acc_go_down = 0
-    max_dev_acc = float('-inf')
-    bestModel = None
-    for t in range(500):
-        for iteration in range(X.shape[0] // N):
-            start = iteration * N
-            end = start + N
-            x = X[start:end, :]
-            y_pred = model(x)
-
-            y = Y[start:end]
-            loss = criterion(y_pred, y)
-            # print t, loss.item()
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        Y_dev_pred = model(X_dev)
-        dev_acc = 100 * \
-            accuracy_score(Y_dev_pred.data.numpy().argmax(
-                axis=1), Y_dev.numpy())
-
-        if dev_acc > max_dev_acc:
-            acc_go_down = 0
-            bestModel = copy.deepcopy(model)
-        else:
-            acc_go_down += 1
-            if acc_go_down == 20:
-                break
-
-        Y_test_pred = model(X_test)
-        print t, 'test accuracy', 100 * \
-            accuracy_score(Y_test_pred.data.numpy().argmax(
-                axis=1), Y_test.numpy())
-
-    writePickle(bestModel, './models/ann/ann_3layer_bow_500')
+    # train
+    ann.fit(trn_encoded, labels)
--- utility.py
+++ utility.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
 class Data:
     WIDTH = 32
     HEIGHT = 32
@@ -15,3 +18,17 @@ def writePickle(var, filepath):
     import cPickle
     with open(filepath, 'wb') as fo:
         cPickle.dump(var, fo)
+
+
+def stratifiedSplitTrainSet(trn_encoded, labels, num_splits):
+    # Split data to train set and dev set
+    split = StratifiedShuffleSplit(
+        n_splits=num_splits, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(trn_encoded, labels):
+        trn_data = trn_encoded[train_index]
+        trn_labels = labels[train_index]
+
+        dev_data = trn_encoded[test_index]
+        dev_labels = labels[test_index]
+
+    return trn_data, trn_labels, dev_data, dev_labels
