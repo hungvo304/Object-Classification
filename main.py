3dd6818be6c88dd16cb7134c074a7c8ed204fae8
MinhHieu <protopro03@gmail.com>
2018-06-11 12:22:06 +0700

    Remove main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from utility import loadPickle
-
-if __name__ == '__main__':
-    databatch1 = loadPickle('./cifar-10-batches-py/data_batch_1')
-    print databatch1['data'].shape
-    print len(databatch1['labels'])
-    label_names = loadPickle('./cifar-10-batches-py/batches.meta')
-    print label_names
